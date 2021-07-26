4b1b715b01eaa9c3f6e7064fa7db91d20a4fcbca
galionix <parfumeland@ya.ru>
2021-07-27 00:15:37 +0300

    test

---
+++ pages/auth/repositories/index.tsx
@@ -0,0 +1,16 @@
+import {
+    ApolloClient,
+    InMemoryCache,
+    gql,
+} from '@apollo/client'
+
+const API_URL: string = 'https://api.github.com/graphql'
+
+export default function Repositories() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -15,19 +15,34 @@ export default function SignIn() {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         // No longer need to cast to any - hooray for react!
         setToken(e.target.value);
+        // console.log("%c 🇰🇪: handleChange -> e.type ", "font-size:16px;background-color:#830fcc;color:white;", e.type)
     }
     const handleSave = (event: React.MouseEvent<HTMLElement>) => {
         setTokenInStore(token)
         router.replace('/')
     }
+    const handleKey = (event: React.KeyboardEvent<HTMLElement>) => {
+        if (event.key === 'Enter') {
+            setTokenInStore(token)
+            router.replace('/')
+        }
+    }
+
+
     return (
         <div
 
 
         >
             <h2>{token}</h2>
-            <input type="text" onChange={handleChange} value={token} />
+            <input
+                type="text"
+                onChange={handleChange}
+                onKeyDown={handleKey}
+                value={token}
+            />
             <button type="submit"
+
                 onClick={handleSave}
             >Save</button>
         </div>
