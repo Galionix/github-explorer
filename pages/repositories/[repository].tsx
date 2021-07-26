98b4e2bbede45ca602c652861a6d8dc45300bca6
galionix <parfumeland@ya.ru>
2021-07-27 00:20:06 +0300

    update

---
+++ pages/repositories/[repository].tsx
@@ -0,0 +1,12 @@
+
+import Header from './../../src/components/Header/Header';
+const Repository = () => {
+    return (
+        <div>
+            <Header />
+            <h1>You're in!</h1>
+        </div>
+    )
+}
+
+export default Repository
